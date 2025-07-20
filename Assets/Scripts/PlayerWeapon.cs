8385d93589808550c794fa7474989b91ff9e3762
Morialle Barr <Moriallekbarr@gmail.com>
2025-07-20 19:13:52 +0300

    Lesson 4.16 - 4.17

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -1,15 +1,23 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerWeapon : MonoBehaviour
 {
-    [SerializeField] private GameObject laser;
+    [SerializeField] private GameObject[] lasers;
+    [SerializeField] RectTransform crosshair;
 
     bool isFiring = false;
+
+    void Start()
+    {
+       Cursor.visible = false;
+    }
 
     private void Update()
     {
         ProcessFiring();
+        MoveCrosshair();
     }
     public void OnFire(InputValue value)
     {
@@ -18,8 +26,16 @@ public class PlayerWeapon : MonoBehaviour
 
     void ProcessFiring()
     {
-        var emissionModule = laser.GetComponent<ParticleSystem>().emission;
-        emissionModule.enabled = isFiring;
+        foreach (GameObject laser in lasers)
+        {
+            var emissionModule = laser.GetComponent<ParticleSystem>().emission;
+            emissionModule.enabled = isFiring;
+        }
+    }
+
+    void MoveCrosshair()
+    {
+        crosshair.position = Input.mousePosition;
     }
 }
   
